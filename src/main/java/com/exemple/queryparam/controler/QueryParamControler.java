b3be636b65cd5cf3af6ee0ade6ac3b4c5c864ba0
Tsiry <hei.tsiry@gmail.com>
2026-03-20 16:36:08 +0300

    feat: add get and verification text/plain

--- src/main/java/com/exemple/queryparam/controler/QueryParamControler.java
+++ src/main/java/com/exemple/queryparam/controler/QueryParamControler.java
@@ -4,25 +4,44 @@ import com.exemple.queryparam.entity.Student;
 import com.exemple.queryparam.service.SaveStudent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class QueryParamControler {
     @GetMapping("/welcome")
     @ResponseBody
-    public String welcome(@RequestParam String name){
+    public String welcome(@RequestParam String name) {
         return "welcome " + name;
     }
 
     @Autowired
     private SaveStudent saveStudent;
+
     @PostMapping("/students")
     @ResponseStatus(HttpStatus.CREATED)
-    public List<Student> students(@RequestBody List<Student> students){
+    public List<Student> students(@RequestBody List<Student> students) {
         saveStudent.addStudents(students);
         return saveStudent.getStudentSaved();
+    }
+
+    @GetMapping("/students")
+    public ResponseEntity<String> getStudents(@RequestHeader("Accept") String acceptHeader) {
+        if (!"text/plain".equals(acceptHeader)) {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)
+                    .body("Format non supporte");
+        }
+
+        List<Student> studentsList = saveStudent.getStudentSaved();
+
+        String names = studentsList.stream()
+                .map(Student::getFirstName)
+                .collect(Collectors.joining(", "));
+
+        return ResponseEntity.ok(names);
     }
 
 }
