ca37d55227bd3f64cc9a47ef3fd46c8d59ae7bc4
Tsiry <hei.tsiry@gmail.com>
2026-03-20 15:15:43 +0300

    feat: initialisation

---
+++ src/main/java/com/exemple/queryparam/QueryParamApplication.java
@@ -0,0 +1,13 @@
+package com.exemple.queryparam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryParamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryParamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exemple/queryparam/QueryParamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemple.queryparam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryParamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
